5ffb5156d2aaa9b1bf983d2ebc125b27631f17e3
chuoru <tranvietthanh0101@gmail.com>
2025-10-09 13:11:56 +0900

    feat: implement joystick_v1, similiar to berkeley humanoid

--- mujoco_playground/config/locomotion_params.py
+++ mujoco_playground/config/locomotion_params.py
@@ -105,6 +105,7 @@ def brax_ppo_config(env_name: str) -> config_dict.ConfigDict:
   elif env_name in (
       "BerkeleyHumanoidJoystickFlatTerrain",
       "BerkeleyHumanoidJoystickRoughTerrain",
+      "HunterJoystickV1",
   ):
     rl_config.num_timesteps = 150_000_000
     rl_config.num_evals = 15
@@ -170,8 +171,7 @@ def brax_ppo_config(env_name: str) -> config_dict.ConfigDict:
       "SpotFlatTerrainJoystick",
       "SpotGetup",
       "SpotJoystickGaitTracking",
-      "HunterJoystick",
-      "HunterJoystickV1",
+      "HunterJoystick"
   ):
     pass  # use default config
   else:
